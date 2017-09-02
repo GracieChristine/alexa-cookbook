3f8bc800db275c61a88c765c194346a18d1a09d0
GracieChristine <graciechristineli@gmail.com>
2017-09-01 23:00:08 -0600

    Underrstanding session attributes, which allow for a personalized message and response.

--- labs/HelloWorld/src/index.js
+++ labs/HelloWorld/src/index.js
@@ -1,53 +1,66 @@
 var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+  var alexa = Alexa.handler(event, context);
 
-    // alexa.dynamoDBTableName = 'YourTableName'; // creates new table for userid:session.attributes
+  // alexa.dynamoDBTableName = 'YourTableName'; // creates new table for userid:session.attributes
 
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('MyIntent');
-        this.emit('WhatsUpIntent');
-        this.emit('AMAZON.HelpIntent');
-        this.emit('AMAZON.StopIntent');
-        this.emit('AMAZON.CancelIntent');
+  'LaunchRequest': function() {
+    this.emit('MyIntent');
+    this.emit('WhatsUpIntent');
+    this.emit('AMAZON.HelpIntent');
+    this.emit('AMAZON.StopIntent');
+    this.emit('AMAZON.CancelIntent');
 
-        this.emit('MyNameIsIntent');
-    },
+    this.emit('MyNameIsIntent');
+  },
 
-    // Tell hello world
-    'MyIntent': function () {
-        this.emit(':tell', 'Hello World from Alexa!');
-    },
+  // Tell hello world
+  'MyIntent': function() {
+    this.emit(':tell', 'Hello World from Alexa!');
+  },
 
-    // Ask what's up
-    'WhatsUpIntent': function () {
-        this.emit(':ask', 'What\'s up from Alexa!');
-    },
+  // Ask what's up
+  'WhatsUpIntent': function() {
+    this.emit(':ask', 'What\'s up from Alexa!');
+  },
 
-    // Help / Help me
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', 'What can Alexa help you with?');
-    },
-
-    // Stop / Stop now
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Alexa will stop now. Let me know when you need me again.');
-    },
-
-    // Cancel / Cancel it
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Confirm cancellation from Alexa.');
-    },
-
-    // My name is {firstname}
-    'MyNameIsIntent': function () {
-      var myName = this.event.request.intent.slots.firstname.value;
-        this.emit(':tell', `Alexa is confirming you are indeed ${myName}.`);
+  // Help / Help me
+  'AMAZON.HelpIntent': function() {
+    let myName = '';
+    if (this.attributes['name']) {
+      myName = this.attributes['name'];
     }
+    this.emit(':ask', `${myName}, what can Alexa help you with?`);
+  },
+
+  // Stop / Stop now
+  'AMAZON.StopIntent': function() {
+    let myName = '';
+    if (this.attributes['name']) {
+      myName = this.attributes['name'];
+    }
+    this.emit(':tell', `Goodbye, ${myName}`, 'try again');
+  },
+
+  // Cancel / Cancel it
+  'AMAZON.CancelIntent': function() {
+    let myName = '';
+    if (this.attributes['name']) {
+      myName = this.attributes['name'];
+    }
+    this.emit(':tell', `Confirm cancelation, ${myName}`);
+  },
+
+  // My name is {firstname}
+  'MyNameIsIntent': function() {
+    let myName = this.event.request.intent.slots.firstname.value;
+    this.attributes['name'] = myName;
+    this.emit(':tell', `Nice to meet you, ${myName}.`);
+  }
 };
