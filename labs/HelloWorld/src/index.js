0fd9f2513c904f9a0f9c8a67d97555feab4d0281
GracieChristine <graciechristineli@gmail.com>
2017-09-01 22:35:44 -0600

    Working on Hello World app. Achieve Initialization and Add New Intents.

--- labs/HelloWorld/src/index.js
+++ labs/HelloWorld/src/index.js
@@ -1,23 +1,45 @@
-const Alexa = require('alexa-sdk');
+var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
 
-    // alexa.appId = 'amzn1.echo-sdk-ams.app.1234';
-
-    ///alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
+    // alexa.dynamoDBTableName = 'YourTableName'; // creates new table for userid:session.attributes
 
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-const handlers = {
+var handlers = {
     'LaunchRequest': function () {
         this.emit('MyIntent');
+        this.emit('WhatsUpIntent');
+        this.emit('AMAZON.HelpIntent');
+        this.emit('AMAZON.StopIntent');
+        this.emit('AMAZON.CancelIntent');
     },
 
+    // Tell hello world
     'MyIntent': function () {
-        this.response.speak('Hello World from Alexa');
-        this.emit(':responseReady')
+        this.emit(':tell', 'Hello World from Alexa!');
+    },
+
+    // Ask what's up
+    'WhatsUpIntent': function () {
+        this.emit(':ask', 'What\'s up from Alexa!');
+    },
+
+    // Help / Help me
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', 'What can Alexa help you with?');
+    },
+
+    // Stop / Stop now
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Alexa will stop now. Let me know when you need me again.');
+    },
+
+    // Cancel / Cancel it
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Confirm cancellation from Alexa.');
     }
 };
